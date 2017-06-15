3cae9e953911b45ae74a5d47ce7a736475ae5957
cbresteau <bresteau.c@gmail.com>
2017-06-15 14:17:42 -0400

    Sauvegarde avant KK

--- lib/acars_gen_impl.cc
+++ lib/acars_gen_impl.cc
@@ -1,19 +1,21 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2017 <+YOU OR YOUR COMPANY+>.
+ * Copyright 2012 Free Software Foundation, Inc.
  *
- * This is free software; you can redistribute it and/or modify
+ * This file is part of GNU Radio
+ *
+ * GNU Radio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * GNU Radio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
+ * along with GNU Radio; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
  * Boston, MA 02110-1301, USA.
  */
@@ -22,157 +24,251 @@
 #include "config.h"
 #endif
 
-#include <gnuradio/io_signature.h>
+#include <gnuradio/thread/thread.h>
 #include "acars_gen_impl.h"
+#include <gnuradio/io_signature.h>
+#include <cstdio>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdexcept>
+#include <stdio.h>
+
+// win32 (mingw/msvc) specific
+#ifdef HAVE_IO_H
+#include <io.h>
+#endif
+#ifdef O_BINARY
+#define	OUR_O_BINARY O_BINARY
+#else
+#define	OUR_O_BINARY 0
+#endif
+// should be handled via configure
+#ifdef O_LARGEFILE
+#define	OUR_O_LARGEFILE	O_LARGEFILE
+#else
+#define	OUR_O_LARGEFILE 0
+#endif
 
 namespace gr {
   namespace Acars_send {
 
-    acars_gen::sptr
-    acars_gen::make(char* file)
+    acars_gen::sptr acars_gen::make(char *filename)
     {
       return gnuradio::get_initial_sptr
-        (new acars_gen_impl(file));
+	(new acars_gen_impl(filename));
     }
 
-    /*
-     * The private constructor
-     */
-    acars_gen_impl::acars_gen_impl(char* file)
-      : gr::sync_block("acars_gen",
-      gr::io_signature::make(0, 0, 0),
-        gr::io_signature::make(1, 1, sizeof(char))),
-        preamble_sync{0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x2B,0x2A,0x16,0x16}
-        /*preamble_sync_bit{11111111,11111111,11111111,11111111,11111111,11111111,11111111,11111111,11111111,11111111,11111111,
-      11111111,11111111,11111111,11111111,11111111,00101011,00101010,00010110,00010110}*/
-{
+    acars_gen_impl::acars_gen_impl(char *filename)
+      : sync_block("acars_gen",
+		      io_signature::make(0, 0, 0),
+		      io_signature::make(1, 1, 1)),
+	d_itemsize(1), d_fp(0), d_new_fp(0), d_repeat(0),
+	d_updated(false)
+    {
+      open(filename, d_repeat);
+      do_update();
+    }
 
-
-  myfile.open(file);
-  int ret = myfile.is_open();
-  printf("Fichier ouvert %d\n",ret);
-}
-    /*
-     * Our virtual destructor.
-     */
     acars_gen_impl::~acars_gen_impl()
     {
-      myfile.close();
-      printf("Fichier fermé\n");
+      if(d_fp)
+        fclose ((FILE*)d_fp);
+      if(d_new_fp)
+        fclose ((FILE*)d_new_fp);
+    }
+
+    bool
+    acars_gen_impl::seek(long seek_point, int whence)
+    {
+      return fseek((FILE*)d_fp, seek_point *d_itemsize, whence) == 0;
     }
 
 
-    /* -------------
-     *  W O R K
-     -------------*/
-
-    int
-    acars_gen_impl::work (int noutput_items,
-        gr_vector_const_void_star &input_items,
-        gr_vector_void_star &output_items)
+    void
+    acars_gen_impl::open(const char *filename, bool repeat)
     {
-      printf("Debut WORK\n");
-      static int sent = 0;
-      printf("%d\n",noutput_items);
-      char *out = (char *) output_items[0];
+      // obtain exclusive access for duration of this function
+      gr::thread::scoped_lock lock(fp_mutex);
 
-      for(int i =0; i < noutput_items; i++){
-      out[i] = 0;
+      int fd;
+
+      // we use "open" to use to the O_LARGEFILE flag
+      if((fd = ::open(filename, O_RDONLY | OUR_O_LARGEFILE | OUR_O_BINARY)) < 0) {
+	perror(filename);
+	throw std::runtime_error("can't open file");
       }
 
-    consume_each (noutput_items);
-    sent += noutput_items;
-    //Compte du nombre de charactères du fichier
+      if(d_new_fp) {
+	fclose(d_new_fp);
+	d_new_fp = 0;
+      }
 
-    char current_char;
-    int c = 0;
-    if (myfile.is_open()){
+      if((d_new_fp = fdopen (fd, "rb")) == NULL) {
+	perror(filename);
+	::close(fd);	// don't leak file descriptor if fdopen fails
+	throw std::runtime_error("can't open file");
+      }
 
-      printf("Fichier BIEN ouvert\n");
-      while(myfile.get(current_char)){
-        //printf("%c\n",current_char);
-        if(current_char == EOF)
-          {
-            break;
-          }
-          c++;
+      d_updated = true;
+      d_repeat = repeat;
+    }
 
-          }
-          printf("Taille : %d\n",c);
-        }
+    void
+    acars_gen_impl::close()
+    {
+      // obtain exclusive access for duration of this function
+      gr::thread::scoped_lock lock(fp_mutex);
 
-    myfile.clear();
-    myfile.seekg(0, std::ios::beg);
+      if(d_new_fp != NULL) {
+	fclose(d_new_fp);
+	d_new_fp = NULL;
+      }
+      d_updated = true;
+    }
 
-    unsigned char message[c-2]={0}; //Ca marche c-2 ??
+    void
+    acars_gen_impl::do_update()
+    {
+      if(d_updated) {
+	gr::thread::scoped_lock lock(fp_mutex); // hold while in scope
 
-    std::string line;
-    getline (myfile,line);
-    std::cout<<line;
-    if (line.find("0x")==0) {
-      printf("Trouvé!\n");
+	if(d_fp)
+	  fclose(d_fp);
 
-    int i = 0;
+	d_fp = d_new_fp;    // install new file pointer
+	d_new_fp = 0;
+	d_updated = false;
+      }
+    }
 
-    for ( std::string::iterator it=(line.begin()+2); it!=line.end(); ++it){
-        unsigned char hex = *it;
-        unsigned char conv;
+    int
+    acars_gen_impl::work(int noutput_items,
+			   gr_vector_const_void_star &input_items,
+			   gr_vector_void_star &output_items)
+    {
+      char *o = (char*)output_items[0];
+      int i;
+      int size = noutput_items;
 
-  printf("Conversion en cours %c\n",c);
+      char hex;
+
+
+      do_update();       // update d_fp is reqd
+      if(d_fp == NULL)
+	throw std::runtime_error("work with file not open");
+
+      gr::thread::scoped_lock lock(fp_mutex); // hold for the rest of this function
+
+      while(size) {
+
+	i = fread(&hex, d_itemsize, 1, (FILE*)d_fp);
+
         switch(toupper(hex))
     {
 
-        case '0': conv = 0;
+        case '0': *o = 0<<4;
         break;
-        case '1': conv = 1;
+        case '1': *o = 1<<4;
         break;
-        case '2': conv = 2;
+        case '2': *o = 2<<4;
         break;
-        case '3': conv = 3;
+        case '3': *o = 3<<4;
         break;
-        case '4': conv = 4;
+        case '4': *o = 4<<4;
         break;
-        case '5': conv = 5;
+        case '5': *o = 5<<4;
         break;
-        case '6': conv = 6;
+        case '6': *o = 6<<4;
         break;
-        case '7': conv = 7;
+        case '7': *o = 7<<4;
         break;
-        case '8': conv = 8;
+        case '8': *o = 8<<4;
         break;
-        case '9': conv = 9;
+        case '9': *o = 9<<4;
         break;
-        case 'A': conv = 10;
+        case 'A': *o = 10<<4;
         break;
-        case 'B': conv = 11;
+        case 'B': *o = 11<<4;
         break;
-        case 'C': conv = 12;
+        case 'C': *o = 12<<4;
         break;
-        case 'D': conv = 13;
+        case 'D': *o = 13<<4;
         break;
-        case 'E': conv = 14;
+        case 'E': *o = 14<<4;
         break;
-        case 'F': conv = 15;
+        case 'F': *o = 15<<4;
         break;
     }
 
+    i = fread(&hex, d_itemsize, 1, (FILE*)d_fp);
 
-    message[i/2]= message[i/2] | (conv<< (4*(1-i%2)));
-    ++i;
+          switch(toupper(hex))
+      {
 
+          case '0': *o |= 0;
+          break;
+          case '1': *o |= 1;
+          break;
+          case '2': *o |= 2;
+          break;
+          case '3': *o |= 3;
+          break;
+          case '4': *o |= 4;
+          break;
+          case '5': *o |= 5;
+          break;
+          case '6': *o |= 6;
+          break;
+          case '7': *o |= 7;
+          break;
+          case '8': *o |= 8;
+          break;
+          case '9': *o |= 9;
+          break;
+          case 'A': *o |= 10;
+          break;
+          case 'B': *o |= 11;
+          break;
+          case 'C': *o |= 12;
+          break;
+          case 'D': *o |= 13;
+          break;
+          case 'E': *o |= 14;
+          break;
+          case 'F': *o |= 15;
+          break;
+      }
+
+	size -= i;
+	o += i * d_itemsize;
+
+	if(size == 0)		// done
+	  break;
+
+	if(i > 0)			// short read, try again
+	  continue;
+
+	// We got a zero from fread.  This is either EOF or error.  In
+	// any event, if we're in repeat mode, seek back to the beginning
+	// of the file and try again, else break
+	if(!d_repeat)
+	  break;
+
+	if(fseek ((FILE *) d_fp, 0, SEEK_SET) == -1) {
+	  fprintf(stderr, "[%s] fseek failed\n", __FILE__);
+	  exit(-1);
+	}
+      }
+printf("j'ai finis %d; %d\n", size, noutput_items);
+      if(size > 0) {	     		// EOF or error
+	if(size == noutput_items)       // we didn't read anything; say we're done
+	  return -1;
+	return noutput_items - size;	// else return partial result
+      }
+
+      return noutput_items;
     }
-  }
 
-  for(int i=0 ; i < c ; i ++ ){
-        std::cout << message[i];
-  }
-
-  memcpy(out,preamble_sync,20*sizeof(char));
-  memcpy(out+20,message,c*sizeof(char));
-  return noutput_items;
-  return -1;
-
-    }
-
-  } /* namespace Acars_send */
+  } /* namespace blocks */
 } /* namespace gr */
--- lib/acars_gen_impl.h
+++ lib/acars_gen_impl.h
@@ -1,54 +1,60 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2017 <+YOU OR YOUR COMPANY+>.
+ * Copyright 2012 Free Software Foundation, Inc.
  *
- * This is free software; you can redistribute it and/or modify
+ * This file is part of GNU Radio
+ *
+ * GNU Radio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * GNU Radio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
+ * along with GNU Radio; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
  * Boston, MA 02110-1301, USA.
  */
 
-#ifndef INCLUDED_ACARS_SEND_ACARS_GEN_IMPL_H
-#define INCLUDED_ACARS_SEND_ACARS_GEN_IMPL_H
+#ifndef INCLUDED_BLOCKS_ACARS_GEN_IMPL_H
+#define INCLUDED_BLOCKS_ACARS_GEN_IMPL_H
 
 #include <Acars_send/acars_gen.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fstream>
-#include <iomanip>
-#include <sstream>
-#include <string.h>
+#include <boost/thread/mutex.hpp>
 
 namespace gr {
   namespace Acars_send {
 
     class acars_gen_impl : public acars_gen
     {
-     private:
-       std::ifstream myfile;
-       float preamble_sync[20];
+    private:
+      size_t d_itemsize;
+      FILE *d_fp;
+      FILE *d_new_fp;
+      bool d_repeat;
+      bool d_updated;
+      boost::mutex fp_mutex;
 
-     public:
-      acars_gen_impl(char* file);
+      void do_update();
+
+    public:
+      acars_gen_impl(char *filename);
       ~acars_gen_impl();
 
-      // Where all the action really happens
+      bool seek(long seek_point, int whence);
+      void open(const char *filename, bool repeat);
+      void close();
+
       int work(int noutput_items,
-         gr_vector_const_void_star &input_items,
-         gr_vector_void_star &output_items);
+	       gr_vector_const_void_star &input_items,
+	       gr_vector_void_star &output_items);
     };
 
-  } // namespace Acars_send
-} // namespace gr
+  } /* namespace blocks */
+} /* namespace gr */
 
-#endif /* INCLUDED_ACARS_SEND_ACARS_GEN_IMPL_H */
+#endif /* INCLUDED_BLOCKS_acars_gen_IMPL_H */
